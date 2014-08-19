6f742d6252860cb986dee6715477550f44de4658
Leandro Cacciagioni <leandro.21.2008@gmail.com>
2014-08-19 18:43:25 +0000

    Fix microbosh task

--- cookbooks/ci_infrastructure_cf/attributes/microbosh.rb
+++ cookbooks/ci_infrastructure_cf/attributes/microbosh.rb
@@ -6,8 +6,8 @@ default[:ci_infrastructure_cf][:jobs][:microbosh].tap do |j|
     j[:provider][:auth_url]= 'https://example.com:5000/v2.0/tokens'
     j[:address][:subnet_id]= 'SUBNET_ID'
     j[:address][:ip]= 'IP'
-    j[:build_cmd] = """
-      rbenv local 1.9.3-p194
-      echo '1\n' | bosh-bootstrap deploy
-    """
+    j[:build_cmd] = '''
+rbenv local 1.9.3-p194
+echo \'1\n\' | bosh-bootstrap deploy
+      '''
 end
